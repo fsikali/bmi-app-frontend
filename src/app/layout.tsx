3b0c964500ceca65f23a3f22e605332efef372a1
fsikali <flemmingsikali499@gmail.com>
2026-03-05 20:29:04 +0300

    delete: SplashScreen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { AuthProvider } from "@/context/AuthContext";
-import SplashScreen from "@/components/SplashScreen";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <AuthProvider>
-            <SplashScreen />
             <Navbar />
             {children}
           </AuthProvider>
